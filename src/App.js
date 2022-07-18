d6abf06646ede48e2d3019c1cad16ef439e5c12f
dlrlseong <leekisung0212@gmail.com>
2022-07-18 12:49:09 +0900

    99.100

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import AddUser from "./components/AddUser/AddUser";
+import UsersList from "./components/AddUser/UsersList";
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users = {[]}/>
     </div>
   );
 }
--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,29 +1,44 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
 function AddUser() {
-  const [enteredUsername, setEnteredUsername] = useState('');
+  const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState();
 
   const changeUsernameHandler = (event) => {
     setEnteredUsername(event.target.value);
-  }
+  };
   const changeAgeHandler = (event) => {
     setEnteredAge(event.target.value);
-  }
+  };
   const addUserHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0)
+      return;
+    if (+enteredAge < 1) return;
     console.log(enteredUsername, enteredAge);
+    setEnteredAge("");
+    setEnteredUsername("");
   };
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={changeUsernameHandler}/>
-        <label htmlFor="username">Age (Years)</label>
-        <input id="username" type="number" onChange={changeAgeHandler}/>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={changeUsernameHandler}
+        />
+        <label htmlFor="age">Age (Years)</label>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={changeAgeHandler}
+        />
         <Button type="sumbit">Add User</Button>
       </form>
     </Card>
